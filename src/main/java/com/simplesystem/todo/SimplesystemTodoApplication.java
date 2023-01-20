391926bdd55a3c3aec4c999b94daf9896d6b5b8d
Benedikt Schneppe <benedikt.schneppe@akdb.de>
2023-01-20 14:56:47 +0100

    initialize spring application with key dependencies

---
+++ src/main/java/com/simplesystem/todo/SimplesystemTodoApplication.java
@@ -0,0 +1,13 @@
+package com.simplesystem.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesystemTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesystemTodoApplication.class, args);
+	}
+
+}
