55f073f017f146db8abbf99cb195d3a92bf1bdc2
Benedikt Schneppe <benedikt.schneppe@akdb.de>
2023-01-21 17:27:29 +0100

    add validation for description modification

--- src/main/java/com/simplesystem/todo/models/TodoModificationDto.java
+++ src/main/java/com/simplesystem/todo/models/TodoModificationDto.java
@@ -1,10 +1,10 @@
 package com.simplesystem.todo.models;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
-import java.time.OffsetDateTime;
 
 /**
  * A DTO for the {@link com.simplesystem.todo.persistence.entity.TodoEntity} entity
  */
-public record TodoModificationDto(String description) implements Serializable {
+public record TodoModificationDto(@NotBlank  String description) implements Serializable {
 }
