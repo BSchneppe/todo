0fc1ed308ad928f83016562bb3faa5751e8f1bc0
Benedikt Schneppe <benedikt.schneppe@akdb.de>
2023-01-22 13:32:16 +0100

    split tests up a little better

--- src/test/java/com/simplesystem/todo/service/TodoServiceTest.java
+++ src/test/java/com/simplesystem/todo/service/TodoServiceTest.java
@@ -2,6 +2,7 @@ package com.simplesystem.todo.service;
 
 import com.simplesystem.todo.mapper.TodoMapper;
 import com.simplesystem.todo.models.TodoCreationDto;
+import com.simplesystem.todo.models.TodoDto;
 import com.simplesystem.todo.models.TodoModificationDto;
 import com.simplesystem.todo.persistence.entity.TodoEntity;
 import com.simplesystem.todo.persistence.repository.TodoRepository;
@@ -51,33 +52,63 @@ class TodoServiceTest {
     void update() {
         //given
         when(todoRepository.findById(VALID_ID)).thenReturn(Optional.of(VALID_TODO_ENTITY));
-        when(todoRepository.findById(PAST_DUE_ID)).thenReturn(Optional.of(PAST_DUE_VALID_TODO_ENTITY));
 
         //when
         TodoModificationDto todoModificationDto = new TodoModificationDto("test");
         tested.update(VALID_ID, todoModificationDto);
-        ThrowableProblem pastDue = assertThrows(ThrowableProblem.class, () -> tested.update(PAST_DUE_ID, todoModificationDto));
-        ThrowableProblem notFound = assertThrows(ThrowableProblem.class, () -> tested.update(3L, todoModificationDto));
 
         //then
         verify(todoMapper, times(1)).partialUpdate(any(TodoModificationDto.class), any(TodoEntity.class));
         verify(todoRepository, times(1)).save(any(TodoEntity.class));
-        Assertions.assertEquals(Status.CONFLICT, pastDue.getStatus());
+    }
+
+    @Test
+    void updateNotFound(){
+        //when
+        TodoModificationDto todoModificationDto = new TodoModificationDto("test");
+        ThrowableProblem notFound = assertThrows(ThrowableProblem.class, () -> tested.update(3L, todoModificationDto));
+        //then
         Assertions.assertEquals(Status.NOT_FOUND, notFound.getStatus());
+    }
+
+    @Test
+    void updatePastDue(){
+        //given
+        when(todoRepository.findById(PAST_DUE_ID)).thenReturn(Optional.of(PAST_DUE_VALID_TODO_ENTITY));
+        //when
+        TodoModificationDto todoModificationDto = new TodoModificationDto("test");
+        ThrowableProblem pastDue = assertThrows(ThrowableProblem.class, () -> tested.update(PAST_DUE_ID, todoModificationDto));
+        //then
+        Assertions.assertEquals(Status.CONFLICT, pastDue.getStatus());
     }
 
     @Test
     void getById() {
         //given
         when(todoRepository.findById(VALID_ID)).thenReturn(Optional.of(VALID_TODO_ENTITY));
-        when(todoRepository.findById(PAST_DUE_ID)).thenReturn(Optional.of(PAST_DUE_VALID_TODO_ENTITY));
         //when
         tested.getById(VALID_ID);
-        tested.getById(PAST_DUE_ID);
-        ThrowableProblem notFound = assertThrows(ThrowableProblem.class, () -> tested.getById(3L));
         //then
         verify(todoRepository, times(1)).findById(VALID_ID);
-        verify(todoRepository, times(1)).findById(PAST_DUE_ID);
+    }
+
+    @Test
+    void getByIdPastDue(){
+        //given
+        when(todoRepository.findById(PAST_DUE_ID)).thenReturn(Optional.of(PAST_DUE_VALID_TODO_ENTITY));
+        //when
+        TodoDto todo = tested.getById(PAST_DUE_ID);
+        //then
+        Assertions.assertEquals(PAST_DUE_VALID_TODO_ENTITY.getDescription(), todo.description());
+        Assertions.assertEquals(PAST_DUE_VALID_TODO_ENTITY.getDueAt(), todo.dueAt());
+    }
+
+    @Test
+    void getByIdNotFound(){
+        //given
+        //when
+        ThrowableProblem notFound = assertThrows(ThrowableProblem.class, () -> tested.getById(3L));
+        //then
         Assertions.assertEquals(Status.NOT_FOUND, notFound.getStatus());
     }
 
@@ -85,30 +116,57 @@ class TodoServiceTest {
     void markAsDone() {
         //given
         when(todoRepository.findById(VALID_ID)).thenReturn(Optional.of(VALID_TODO_ENTITY));
-        when(todoRepository.findById(PAST_DUE_ID)).thenReturn(Optional.of(PAST_DUE_VALID_TODO_ENTITY));
         //when
         tested.markAsDone(VALID_ID);
-        ThrowableProblem pastDue = assertThrows(ThrowableProblem.class, () -> tested.markAsDone(PAST_DUE_ID));
-        ThrowableProblem notFound = assertThrows(ThrowableProblem.class, () -> tested.markAsDone(3L));
         //then
         verify(todoRepository, times(1)).save(any(TodoEntity.class));
-        Assertions.assertEquals(Status.CONFLICT, pastDue.getStatus());
-        Assertions.assertEquals(Status.NOT_FOUND, notFound.getStatus());
+    }
 
+    @Test
+    void markAsDonePastDue(){
+        //given
+        when(todoRepository.findById(PAST_DUE_ID)).thenReturn(Optional.of(PAST_DUE_VALID_TODO_ENTITY));
+        //when
+        ThrowableProblem pastDue = assertThrows(ThrowableProblem.class, () -> tested.markAsDone(PAST_DUE_ID));
+        //then
+        Assertions.assertEquals(Status.CONFLICT, pastDue.getStatus());
+    }
+
+    @Test
+    void markAsDoneNotFound(){
+        //given
+        //when
+        ThrowableProblem notFound = assertThrows(ThrowableProblem.class, () -> tested.markAsDone(3L));
+        //then
+        Assertions.assertEquals(Status.NOT_FOUND, notFound.getStatus());
     }
 
     @Test
     void markAsNotDone() {
         //given
         when(todoRepository.findById(VALID_ID)).thenReturn(Optional.of(VALID_TODO_ENTITY));
-        when(todoRepository.findById(PAST_DUE_ID)).thenReturn(Optional.of(PAST_DUE_VALID_TODO_ENTITY));
         //when
         tested.markAsNotDone(VALID_ID);
-        ThrowableProblem pastDue = assertThrows(ThrowableProblem.class, () -> tested.markAsNotDone(PAST_DUE_ID));
-        ThrowableProblem notFound = assertThrows(ThrowableProblem.class, () -> tested.markAsNotDone(3L));
         //then
         verify(todoRepository, times(1)).save(any(TodoEntity.class));
+    }
+
+    @Test
+    void markAsNotDonePastDue() {
+        //given
+        when(todoRepository.findById(PAST_DUE_ID)).thenReturn(Optional.of(PAST_DUE_VALID_TODO_ENTITY));
+        //when
+        ThrowableProblem pastDue = assertThrows(ThrowableProblem.class, () -> tested.markAsNotDone(PAST_DUE_ID));
+        //then
         Assertions.assertEquals(Status.CONFLICT, pastDue.getStatus());
+    }
+
+    @Test
+    void markAsNotDoneNotFound() {
+        //given
+        //when
+        ThrowableProblem notFound = assertThrows(ThrowableProblem.class, () -> tested.markAsNotDone(3L));
+        //then
         Assertions.assertEquals(Status.NOT_FOUND, notFound.getStatus());
     }
 
